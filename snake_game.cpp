68d6cdf76de17e644787cb2ee1f8e588fb16a586
Nguyen Ha Mi <nguyenhami@Nguyens-MacBook-Pro.local>
2025-09-21 21:11:24 +0200

    Commit in-progress creation of Snake class

--- snake_game.cpp
+++ snake_game.cpp
@@ -12,6 +12,11 @@ using namespace std;
 
 /*
 Step 1: Create game grid
+
+Storing the 2D grid data (e.g., a char array).
+Initializing the grid (setting up the walls and empty spaces).
+Printing the grid to the console.
+Providing methods to check if a position is a wall.
 */ 
 
 // Approach 1: 2D array for 20x20 fixed-size grid
@@ -40,8 +45,35 @@ class GameGrid {
         }
     }
 
+    // Print grid with initial positions
+    for (int i = 0; i < grid_height; ++i) {
+        for (int j = 0; j < grid_width; ++j) {
+            cout << game_grid[i][j];
+        }
+        cout << endl;
+    }
+};
+
+// Approach 2: std::vector of std::vector
+// ...
+
+
+/*
+Step 2: Create snake's body
+Storing the snake's body coordinates (e.g., a std::vector of Point structs).
+Handling the snake's movement (updating coordinates based on direction).
+Managing the snake's growth when it eats food.
+Checking for self-collision.
+By defining a struct to store coordinates of body segments.
+Loop through the snakeBody vector. 
+For each Point object in the vector, update the corresponding 
+cell in the gameGrid to the snake body character 'o'.
+*/ 
+
+class Snake {
+
     /*
-    Initialize snake body at a random position by generating random coordinates.
+    Initialize snake's head at a random position by generating random coordinates.
     Valid x-coordinates are from 0 to (grid_height-1). Similarly for y-coordinates.
     However, snake body must not be on 4 walls of the grid. 
     Thus, safe range for snake is x=1 to x=grid_height-2. Similarly for y-coordinates.
@@ -71,15 +103,29 @@ class GameGrid {
     int start_x = get_random_number(min_safe_x; max_safe_x);
     int start_y = get_random_number(min_safe_y; max_safe_y);
 
-    // Print grid with initial positions
-    for (int i = 0; i < grid_height; ++i) {
-        for (int j = 0; j < grid_width; ++j) {
-            cout << game_grid[i][j];
-        }
-        cout << endl;
-    }
+    // Constructor to store snake's starting position
+    Snake(int start_x, int start_y);
+
+    // Define a struct to hold a coordinate point
+    struct point {
+        int x;
+        int y;
+    };
+
+    // Create vector to store multiple `point` objects, representing entire snake body
+    vector<point> snake_body;
 };
 
+
+/*
+Acts as the game manager. It brings together the GameGrid and Snake objects 
+and orchestrates the game's flow.
+Initializing the GameGrid and Snake objects.
+Handling the main game loop.
+Getting user input.
+Calling methods from the Snake and GameGrid classes to update and render the game state.
+Checking for game-over conditions.
+*/
 int main() {
 
 }
